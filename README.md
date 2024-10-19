b3d0f13065dec52d32b122f57c794faba54b448d
Artem Lastochkin <pryanikov010@yandex.ru>
2024-10-19 08:48:35 +0300

    Fix: сделана валидация

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://artemlastochkin.github.io/mesto-project-ff/
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./pages/index.css";
 import { initialCards } from "./scripts/cards";
 import { creatCard, deleteCard, likeCard } from "./components/card";
 import { closePopup, openPopup, clickOverlay } from "./components/modal";
+import { enableValidation, clearValidation } from "./validation";
 
 // глобал const
 const profileTitle = document.querySelector(".profile__title");
@@ -18,9 +19,9 @@ const formTypeEditProfile = document.forms["edit-profile"];
 const formTypeNewCard = document.forms["new-place"];
 
 const inputNamePopupTypeEdit = document.forms["edit-profile"].elements.name;
-
 const inputDescriptionPopupTypeEdit =
   document.forms["edit-profile"].elements.description;
+
 const inputPlaceNameTypeNewCard =
   document.forms["new-place"].elements["place-name"];
 const inputlinkTypeNewCard = document.forms["new-place"].elements.link;
@@ -31,6 +32,30 @@ const textPopupTypeImage = popupTypeImage.querySelector(".popup__caption");
 const placesList = document.querySelector(".places__list");
 const popupPseudoArr = document.querySelectorAll(".popup");
 const popupArr = Array.from(popupPseudoArr);
+
+const objectClearValidation = {
+  keyIdSpanError: "__error_message",
+  keyPopapInput: ".popup__input",
+  keyDeleteBtn: "[type=submit]"
+}
+
+const validationSetting = {
+  keyPopapInput: ".popup__input",
+  keyPopupForm: ".popup__form",
+  keyIdSpanError: "__error_message",
+  keyDeleteBtn: "[type=submit]"
+}
+
+fetch('https://nomoreparties.co/v1/wff-cohort-24/users/me', {
+  headers: {
+    authorization: '020c57ef-afc5-4efb-96e2-9de924c090b7'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+    profileTitle.textContent = result.name
+  })
 
 // обработчик клика по карточке
 function openPopupCardImg(evt) {
@@ -61,21 +86,25 @@ function addNewCardFormSubmit(evt) {
   closePopup(popupTypeNewCard);
 }
 
+// включить валидацию форм
+enableValidation(validationSetting);
+
 // клик по кнопке редактировать профиль
 profileEditButton.addEventListener("click", function (evt) {
   openPopup(popupTypeEdit);
   inputNamePopupTypeEdit.value = profileTitle.textContent;
   inputDescriptionPopupTypeEdit.value = profileDescription.textContent;
+  clearValidation(formTypeEditProfile, objectClearValidation);
 });
 
 // клик по кнопке добавить карточку
-profileAddButton.addEventListener("click", (evt) =>
-  openPopup(popupTypeNewCard)
-);
+profileAddButton.addEventListener("click", function (evt) {
+  openPopup(popupTypeNewCard);
+  clearValidation(formTypeNewCard, objectClearValidation);
+});
 
 // слушатели submit для форм редактирования профиля
 formTypeEditProfile.addEventListener("submit", editProfileFormSubmit);
-
 formTypeNewCard.addEventListener("submit", addNewCardFormSubmit);
 
 // +класс при старте, +слушатель на кнопку крестик, +слушатель на оверлей popup
@@ -91,3 +120,21 @@ popupArr.forEach(function (item) {
 initialCards.forEach(function (item) {
   placesList.append(creatCard(item, deleteCard, likeCard, openPopupCardImg));
 });
+
+
+// ================================================================
+
+
+
+// fetch('https://nomoreparties.co/v1/wff-cohort-24/cards', {
+//   headers: {
+//     authorization: '020c57ef-afc5-4efb-96e2-9de924c090b7'
+//   }
+// })
+//   .then(res => res.json())
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+
+
